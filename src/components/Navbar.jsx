4ab546f1e1c9e124702cb68dc2cfcce4d3f4047d
Grandelisn <grandelisn@gmail.com>
2021-02-10 18:48:06 -0500

    mvp y'all

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const Navbar = (props) => {
+import {useDarkMode} from '../hooks/useDarkMode'
+const Navbar = () => {
+  const [darkMode, setDarkMode] = useDarkMode(false);
   const toggleMode = e => {
     e.preventDefault();
-    props.setDarkMode(!props.darkMode);
+    setDarkMode(!darkMode);
   };
   return (
     <nav className="navbar">
@@ -11,11 +13,10 @@ const Navbar = (props) => {
       <div className="dark-mode__toggle">
         <div
           onClick={toggleMode}
-          className={props.darkMode ? 'toggle toggled' : 'toggle'}
+          className={darkMode ? 'toggle toggled' : 'toggle'}
         />
       </div>
     </nav>
   );
 };
-
 export default Navbar;
